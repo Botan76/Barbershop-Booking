0f7880b40e647576868b875121a65c9ddb548857
Botan Majeed <botan_art@hotmail.com>
2025-08-04 20:32:34 +0200

    Initial Commit

---
+++ Barbershop Booking/Barbershop_BookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barbershop_BookingApp.swift
+//  Barbershop Booking
+//
+//  Created by Bootan Majeed on 2025-08-04.
+//
+
+import SwiftUI
+
+@main
+struct Barbershop_BookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Barbershop BookingTests/Barbershop_BookingTests.swift
@@ -0,0 +1,17 @@
+//
+//  Barbershop_BookingTests.swift
+//  Barbershop BookingTests
+//
+//  Created by Bootan Majeed on 2025-08-04.
+//
+
+import Testing
+@testable import Barbershop_Booking
+
+struct Barbershop_BookingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
